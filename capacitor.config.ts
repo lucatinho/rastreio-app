dba2c66e6e3aaec4465d860c2d1136555a17261f
Edjunior Lucatte <edjunior.lucatte@parcas.com.br>
2024-01-17 13:52:22 -0300

    feat: ajustes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'rastreio',
+  appName: 'Rastreio',
   webDir: 'www',
   server: {
     androidScheme: 'https'
